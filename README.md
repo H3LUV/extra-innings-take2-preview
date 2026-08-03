f5684f6a3872423290c8f51d110d18a8454bd71f
H3LUV <ahrvks@gmail.com>
2026-08-04 02:05:32 +0900

    Deploy Today MLB v2 loader

--- README.md
+++
@@ -1,2 +0,0 @@
-# extra-innings-take2-preview
-EXTRA INNINGS TAKE 2 web preview
--- travel-guide-admin/hotfix-v7.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  encodeData = async function encodeDataV7(data) {
-    const raw = new TextEncoder().encode(JSON.stringify(data));
-    return `n${bytesToB64url(raw)}`;
-  };
-})();
